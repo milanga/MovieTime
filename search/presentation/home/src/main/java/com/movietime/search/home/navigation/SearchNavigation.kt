f220fa6e6ab619b7dbfcc97a93697b6ce468f8a5
gustavotorrecilla <torrecillagustavo@gmail.com>
2024-08-15 11:55:21 +0100

    fix search

--- search/presentation/home/src/main/java/com/movietime/search/home/navigation/SearchNavigation.kt
+++ search/presentation/home/src/main/java/com/movietime/search/home/navigation/SearchNavigation.kt
@@ -1,12 +1,11 @@
 package com.movietime.search.home.navigation
 
-import androidx.compose.animation.*
-import androidx.compose.animation.core.tween
-import androidx.navigation.*
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
-import com.movietime.movie.detail.ui.MovieDetailRoute
 import com.movietime.search.home.ui.SearchHome
+
+private const val SEARCH_ORIGIN = "search"
 
 sealed class SearchDestinations(val route: String) {
     object HOME : SearchDestinations("search/home")
@@ -32,8 +31,8 @@ fun NavGraphBuilder.searchGraph(
     ) {
         composable(SearchDestinations.HOME.route) {
             SearchHome(
-                onMovieSelected = { movieId -> navigateToRoute("${SearchDestinations.MOVIE_DETAIL.route}/$movieId")},
-                onTvShowSelected = { tvShowId -> navigateToRoute("${SearchDestinations.TV_SHOW_DETAIL.route}/$tvShowId")},
+                onMovieSelected = { movieId -> navigateToRoute("${SearchDestinations.MOVIE_DETAIL.route}/$movieId/$SEARCH_ORIGIN")},
+                onTvShowSelected = { tvShowId -> navigateToRoute("${SearchDestinations.TV_SHOW_DETAIL.route}/$tvShowId/$SEARCH_ORIGIN")},
                 onPersonSelected = { }
             )
         }
